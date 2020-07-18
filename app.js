fd46f4ac566b3fb9d68835d4bfd095efa2511939
Carlos de la Cruz <carlosdelacruzacosta4@gmail.com>
2020-07-17 20:14:36 -0500

    clean the backpack

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
+//Values for timer()
 let countdown;
+let timeSet = 1500;
+
+//Elements from the DOM
 const display = document.querySelector("#timer");
 const allbtns = document.querySelectorAll(".set");
 const body = document.querySelector("body");
 const boton = document.querySelector("#btn");
-let timeSetP = 1500;
-let timeSetS = 0;
-let timesetL = 0;
 
-boton.addEventListener("click", callClosure());
+const pomodoro = document.querySelector("#pomodoro");
+const short = document.querySelector("#short");
+const long = document.querySelector("#long");
+
+// The varibale timeSet is goin to be what's passed onto timer()
+//I gotta update timeSet to change when:
+// 1.- I click on pomodoro/short/long
+// 2.- I change the amount of minutes the session will take (settings)
+
+function setTime(valueInMinutes) {
+  return parseInt(valueInMinutes * 60);
+}
+
+let variable;
+let prueba10 = callClosure();
+
+boton.addEventListener("click", prueba10);
 
 function callClosure() {
-  let variable;
-  console.log(variable);
   return function aClousure(e) {
+    console.log(e.target.color);
     if (e.target.textContent === "START" && variable === undefined) {
       e.target.textContent = "STOP";
-      timer(timeSetP);
+      timer(timeSet);
+      console.log(`case 1 ${variable}`);
     } else if (e.target.textContent === "STOP") {
       e.target.textContent = "START";
       clearInterval(countdown);
@@ -31,28 +48,45 @@ function callClosure() {
       );
       minutos = parseInt(minutos.join(""));
       variable = minutos * 60 + segundos;
-      console.log(variable);
+      console.log(`case 2 ${variable}`);
     } else if (e.target.textContent === "START" && variable !== undefined) {
-      console.log("sup");
       e.target.textContent = "STOP";
       clearInterval(countdown);
       timer(variable);
+      console.log(`case 3 ${variable}`);
     }
   };
 }
 
 body.addEventListener("click", (e) => {
   if (e.target.id === "short") {
+    clearInterval(countdown);
+    variable = undefined;
     body.style.backgroundColor = "#4ca6a9";
     btn.style.color = "#4ca6a9";
-    display.textContent = "5:00";
+    display.textContent = `${short.dataset.min}:00`;
+    timeSet = setTime(short.dataset.min);
+    console.log(timeSet);
+    boton.textContent = "START";
   } else if (e.target.id === "pomodoro") {
+    clearInterval(countdown);
+    variable = undefined;
     body.style.backgroundColor = "#f05b56";
     btn.style.color = "#f05b56";
+    display.textContent = `${pomodoro.dataset.min}:00`;
+    timeSet = setTime(pomodoro.dataset.min);
+    console.log(timeSet);
+    boton.textContent = "START";
   } else if (e.target.id === "long") {
+    clearInterval(countdown);
+    variable = undefined;
     body.style.backgroundColor = "#498fc1";
     btn.style.color = "#498fc1";
-    display.textContent = "10:00";
+    display.textContent = `${long.dataset.min}:00`;
+    timeSet = setTime(long.dataset.min);
+    console.log(timeSet);
+
+    boton.textContent = "START";
   }
 });
 
@@ -62,6 +96,7 @@ allbtns.forEach((element) => {
     e.target.classList.add("active");
   });
 });
+
 function checkActive() {
   allbtns.forEach((element) => {
     if (element.classList.contains("active")) {
